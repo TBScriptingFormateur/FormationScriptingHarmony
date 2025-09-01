7838d0dc9e33be899485541df64a289b42a6cda0
Frappiche <marionfrappe@orange.fr>
2025-09-01 16:44:33 +0200

    formules de politesse

--- exercices/bouton_test.js
+++ exercices/bouton_test.js
@@ -1,5 +1,9 @@
 function formuleDePolitesse(){ 
-    MessageBox.information("BONJOUR")
-    MessageBox.information("MERCI")
-    MessageBox.information("AU REVOIR")
+    var salutation ="Bonjour"
+    var remerciement = "Merci"
+    var congé = "Au revoir"
+    var nom = "Alexandre"
+    MessageBox.information(salutation nom)
+    MessageBox.information(remerciement nom)
+    MessageBox.information(congé nom)
 }       
