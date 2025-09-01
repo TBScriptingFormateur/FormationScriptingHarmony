816103be99fd1e61c949f855d675b84cfeef40ea
julialeblanc16 <yoshmax@hotmail.fr>
2025-09-01 16:41:45 +0200

    Update bouton_test.js

--- exercices/bouton_test.js
+++ exercices/bouton_test.js
@@ -1,7 +1,7 @@
 function myfunc(){ 
         var texte = "Salut les nazes"
-        MessageBox.information("texte!") 
-        MessageBox.information("texte!") 
-        MessageBox.information("texte!") 
+        MessageBox.information(texte!) 
+        MessageBox.information(texte!) 
+        MessageBox.information(texte!) 
         }
 
