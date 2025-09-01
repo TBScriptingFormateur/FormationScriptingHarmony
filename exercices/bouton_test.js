245bc32ead166620adb7bb00394ddf8f5e4e870e
julialeblanc16 <yoshmax@hotmail.fr>
2025-09-01 16:54:25 +0200

    Update bouton_test.js

--- exercices/bouton_test.js
+++ exercices/bouton_test.js
@@ -1,7 +1,9 @@
+var texte = "Salut les nazes"
+ var nombre = 17
+var notice = "pas de caractère spéciaux ni d'espaces dans les noms"
+
 function myfunc(){ 
-        var texte = "Salut les nazes"
-        var nombre = 17
-        var notice = "pas de caractère spéciaux ni d'espaces dans les noms"
-        MessageBox.information(texte + notice)
+        MessageBox.information(texte + " " + notice)
         }
 
+mufunc()
