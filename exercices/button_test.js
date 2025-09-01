79eec2e6fdce2841ca58c21195c0228dabbe7921
amunells <229730352+amunells@users.noreply.github.com>
2025-09-01 15:02:51 +0200

    Creattion du button test
    Pour tester different functions dans un meme script.

---
+++ exercices/button_test.js
@@ -0,0 +1,12 @@
+//Function to say yow!
+function myfuncA(){
+    MessageBox.information("Yow !")
+}
+//Function to say Salut!
+function myfuncB(){
+    MessageBox.information("Salut !")
+}
+// Function to say Ça va?
+function myfuncC(){
+    MessageBox.information("Ça va ?!")
+}
