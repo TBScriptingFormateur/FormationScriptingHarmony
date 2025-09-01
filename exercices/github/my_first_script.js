a887af0fb8aa785fe7520cc1e53ab593bd36472e
Frappiche <marionfrappe@orange.fr>
2025-09-01 14:37:36 +0200

    Update my_first_script.js

--- exercices/github/my_first_script.js
+++ exercices/github/my_first_script.js
@@ -1,4 +1,8 @@
 function myfunc(){
         MessageBox.information("my fir") 
+<<<<<<< Updated upstream
         MessageLog.trace("Hello from the log") 
+=======
+        MessageLog.trace("Yopiyopla") 
+>>>>>>> Stashed changes
 }
