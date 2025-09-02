1d4e0b54c7f23e1749d73d9f2cd48ba8034a1914
julialeblanc16 <yoshmax@hotmail.fr>
2025-09-02 12:03:23 +0200

    Update bases_variables_string1.js

---
+++ exercices/bases_variables_string1.js
@@ -0,0 +1,20 @@
+function direBonjour(){
+    var bonjour = "Bonjour "
+    var feu = "🔥"
+    MessageBox.information(bonjour+ " " + feu)
+
+    feu += "🔥"
+
+    MessageBox.information(bonjour+ " " + feu)
+    feu += "🔥"
+
+         MessageBox.information(bonjour+ " " + feu)
+     feu += "🔥"
+
+         MessageBox.information(bonjour+ " " + feu)
+    feu += "🔥"
+
+         MessageBox.information(bonjour+ " " + feu)
+    feu += "🔥"
+     
+}
