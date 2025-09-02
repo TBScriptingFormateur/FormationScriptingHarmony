9b91158bae664b2f0377af276aeb01352c6089c0
julialeblanc16 <yoshmax@hotmail.fr>
2025-09-02 11:31:28 +0200

    Update bases_variables_int_float.js

--- exercices/bases_variables_int_float.js
+++ exercices/bases_variables_int_float.js
@@ -1,9 +1,9 @@
 function variable_int(){
 
-    var coucou = 30
-    coucou = "julia"
+    var coucou = "julia" 
+    var truc = 20
     var my_var = 1
-    MessageBox.information(coucou + my_var)
+    MessageBox.information(coucou + my_var + truc)
 
     // declater une nouvelle variable et afficher son contenu deux fois 
     // declater une nouvelle variable changer sa valeur et afficher son contenu 
@@ -12,9 +12,9 @@ function variable_int(){
 
 function variable_int_operators_1(){
 
-    var my_var = 1 + 2
-
-    MessageBox.information(my_var)
+    const result = 30
+    
+    MessageBox.information(result)
 
     // declarez deux variables , stocker le resultat de leur addition dans une troisième variable et afficher son contenu 
 
@@ -32,9 +32,13 @@ function variable_int_operators_2(){
 
     var nb_secondes_dans_une_minutes = 60
     var nb_minutes_dans_une_heure = 60
-    var nb_heure_par_jour = 60
+    var nb_heure_par_jour = 24
 
-    var nb_secondes_dans_un_jour = 0
+    var nb_secondes_dans_un_jour = nb_secondes_dans_une_minutes*nb_minutes_dans_une_heure*nb_heure_par_jour
+
+    MessageBox.information(nb_secondes_dans_un_jour)
+
+
 
     // calculer puis afficher la dernière variable 
 
@@ -48,6 +52,8 @@ function variable_int_operators_3(){
     var potion_de_soin = +300
     var points_de_vie_de_base = 30
 
+    var points_de_vie_finaux = points_de_vie_de_base + degats_cone_de_glace + degats_boule_de_feu + potion_de_soin
+    MessageBox.information(points_de_vie_finaux)
     // calculer la variable "point_de_vie_finaux" du hero après les deux attaques et la potion de soin 
 
 
@@ -62,6 +68,8 @@ function variable_float(){
     var longeur_barbe_de_bifur = 45
     var longeur_barbe_de_bombur = 5
 
+    var longueurTotale = longeur_barbe_de_fili + longeur_barbe_de_balin + longeur_barbe_de_thorin + longeur_barbe_de_kili + longeur_barbe_de_bifur + longeur_barbe_de_bombur
+        MessageBox.information(longueurTotale/6)
     // calculez la longueur moyenne de la barbe des nains présents dans Le Hobbit
 
 
