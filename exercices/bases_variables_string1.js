e2396dcc65efb7c3e134623e0c3c2cd3d48b8518
TBScriptingFormateur <alarriger@gmail.com>
2025-09-02 12:21:10 +0200

    Update bases_variables_string1.js

--- exercices/bases_variables_string1.js
+++ exercices/bases_variables_string1.js
@@ -3,5 +3,27 @@ function dire_bonjour(){
     var age = 0
     age +=1
 
+<<<<<<< Updated upstream
     MessageBox.information(age)
 }
+=======
+    var addonLove = emot + 1 // "❤️‍🔥✨✨✨✨✨✨✨❤️‍🔥1"
+
+    MessageBox.information (addonLove) 
+    addonLove += emot 
+
+    MessageBox.information (addonLove) 
+    addonLove += emot 
+
+    MessageBox.information (addonLove) 
+    addonLove += emot 
+
+    MessageBox.information (addonLove) 
+    addonLove += emot 
+
+    MessageBox.information (addonLove)
+    
+// affiche le message de bonjour
+}
+
+>>>>>>> Stashed changes
