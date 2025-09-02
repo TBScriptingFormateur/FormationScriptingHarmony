892e19a0a8cb4be8c4e164983c58c8d5dfb286bd
Frappiche <marionfrappe@orange.fr>
2025-09-02 11:30:44 +0200

    barbuuu

--- exercices/bases_variables_int_float.js
+++ exercices/bases_variables_int_float.js
@@ -34,7 +34,7 @@ function variable_int_operators_1(){
 
     var A = 2
     var B = 5
-    my_var = B / A
+    var my_var = B / A
 
     MessageBox.information(my_var)
 
@@ -54,36 +54,46 @@ function variable_int_operators_2(){
 
     var nb_secondes_dans_une_minutes = 60
     var nb_minutes_dans_une_heure = 60
-    var nb_heure_par_jour = 60
+    var nb_heure_par_jour = 24
 
-    var nb_secondes_dans_un_jour = 0
+    var nb_secondes_dans_un_jour = nb_heure_par_jour * nb_minutes_dans_une_heure * nb_secondes_dans_une_minutes
 
+    MessageBox.information(nb_secondes_dans_un_jour)
+    
     // calculer puis afficher la dernière variable 
-
+    
 }
 
 
 function variable_int_operators_3(){
-
+    
     var degats_cone_de_glace = -100
     var degats_boule_de_feu = -150
     var potion_de_soin = +300
     var points_de_vie_de_base = 30
-
+    
+    var point_de_vie_finaux = points_de_vie_de_base + degats_cone_de_glace + degats_boule_de_feu + potion_de_soin
+    
+    MessageBox.information(point_de_vie_finaux)
+    
     // calculer la variable "point_de_vie_finaux" du hero après les deux attaques et la potion de soin 
-
-
+    
+    
 }
 
 function variable_float(){
-
+    
     var longeur_barbe_de_fili = 6
     var longeur_barbe_de_balin = 40
     var longeur_barbe_de_thorin = 10
     var longeur_barbe_de_kili = 5
     var longeur_barbe_de_bifur = 45
     var longeur_barbe_de_bombur = 5
-
+    var nombre_de_barbu = 6
+    
+    var longueur_moyenne_barbe = (longeur_barbe_de_fili + longeur_barbe_de_balin + longeur_barbe_de_thorin + longeur_barbe_de_kili + longeur_barbe_de_bifur +longeur_barbe_de_bombur)/nombre_de_barbu
+    
+    MessageBox.information(longueur_moyenne_barbe)
     // calculez la longueur moyenne de la barbe des nains présents dans Le Hobbit
 
 
