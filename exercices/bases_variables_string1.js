556eadbe4a7145825ed11d13da401569dfcdfc8a
Frappiche <marionfrappe@orange.fr>
2025-09-03 09:01:01 +0200

    lecon hier

--- exercices/bases_variables_string1.js
+++ exercices/bases_variables_string1.js
@@ -1,38 +1,22 @@
 function dire_bonjour(){
-    var salutation = "Bonjour"
-    var nom = " Marion"
+    var salutation = 'Bonjour'
+    var nom = " Marion \"charactère échappé\""
     var ville = " Angoulême \n"
     var emoji = "😈"
-    var dire_bonjour = salutation + nom + " de" + ville + emoji
+    var dire_bonjour = salutation + nom + " de" + ville + emoji 
 
     MessageBox.information(dire_bonjour)
 }
 
 function incendie(){
     var comete = "☄️"
-    var feu = "🔥"
+    var feu = ["🔥","🌈","☔️","🍐"]
+   
+    for(var index in feu){
 
-    var incendie = comete
-
-    MessageBox.information(incendie)
-
-
-    feu += feu
-    MessageBox.information(feu)
-
-    feu += feu
-    MessageBox.information(feu)
-
-    feu += feu
-    MessageBox.information(feu)
-
-    feu += feu
-    MessageBox.information(feu)
-
-    feu += feu
-    MessageBox.information(feu)
-
-
+        MessageBox.information(feu[index])
+    }
+ 
 
 
 }
---
+++ exercices/bases_variables_string2.js
@@ -0,0 +1,9 @@
+function string_2(){ 
+    var nodes = selection.selectedNodes()
+    for( var index in nodes)
+    MessageBox.information("<3 "+ nodes[index])
+    //MessageBox.information("Bravo! vous avez sélectionné "+ nodes.length +" nodes")
+    //MessageBox.information("<3"+nodes.join("\n <3"))
+    
+}
+   
