98a07f993d208a15a68fac4462666d9ff2913fd5
julialeblanc16 <yoshmax@hotmail.fr>
2025-09-03 09:58:58 +0200

    Update harmony_nodes.js

--- exercices/harmony_nodes.js
+++ exercices/harmony_nodes.js
@@ -7,5 +7,8 @@ function how_many_pegs(){
 function align_nodes_horizontaly(){
 
     // aligner les nodes dans la nodeview sur l'axe x 
+    
+    var nodes = selection.selectedNodes()
+    MessageLog.trace(nodes[0])
 
 }
