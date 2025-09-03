f640121315ded0357d566b2d688ae1e4a0b2416a
julialeblanc16 <yoshmax@hotmail.fr>
2025-09-03 14:47:19 +0200

    Update harmony_nodes.js

--- exercices/harmony_nodes.js
+++ exercices/harmony_nodes.js
@@ -8,7 +8,39 @@ function align_nodes_horizontaly(){
 
     // aligner les nodes dans la nodeview sur l'axe x 
     
-    var nodes = selection.selectedNodes()
-    MessageLog.trace(nodes[0])
+    var firstNode = selection.selectedNodes()[0]
 
+    var nodeX = node.coordX(firstNode)
+    var nodeY = node.coordY(firstNode)
+    
+    //node.setCoord(firstNode,0,0);
+
+    MessageLog.trace("\n Node     👉 " + firstNode[0] + "\n Coord X👉 " + nodeX + "\n Coord Y👉 " + nodeY)
+
+    //var translate = node.width(firstNode)
+    var translate = 10
+    var newX = nodeX + translate
+
+    node.setCoord(firstNode,newX,nodeY);
+
+
+    
 }
+
+function align () {
+
+    var select = selection.selectedNodes()
+    var firstNode = select[0]
+    var firstNodeY = node.coordY(firstNode)
+    var firstNodeX = node.coordX(firstNode)
+    var newX = firstNodeX
+
+    for (var index in select) {
+        var currentNode = select[index]
+        var currentNodewidth = node.width(currentNode)
+        newX += node.width(select[index-1])+10
+        node.setCoord(currentNode,newX, firstNodeY)
+
+    }
+}
+
