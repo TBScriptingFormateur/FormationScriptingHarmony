2b7bf2d7067281322f3387fdde901b4fc636ad02
TBScriptingFormateur <alarriger@gmail.com>
2025-09-03 09:34:44 +0200

    j

--- exercices/bases_variables_string2.js
+++ exercices/bases_variables_string2.js
@@ -1,5 +1,18 @@
 function string_2(){
-
-
+        
+    var nodes = selection.selectedNodes()
+    var flammes = 0
     
+    for (var index in nodes){
+        flammes+=1
+    }
+
+    MessageBox.information(flammes)
+
+    // bool 
+    // objects 
+    // function 
+    // classes 
+
+
 }
