b93a272f078aacef431660aa45f444484cd8c70d
Ju-rigg <ju.rigg.harmony@gmail.com>
2025-09-03 09:58:37 +0200

    Update harmony_nodes.js

--- exercices/harmony_nodes.js
+++ exercices/harmony_nodes.js
@@ -8,4 +8,8 @@ function align_nodes_horizontaly(){
 
     // aligner les nodes dans la nodeview sur l'axe x 
 
+    var Nodes = selection.selectedNodes()
+
+
+    MessageLog.trace(Nodes.index[0])
 }
