188147e5623dbaa2eed0f60d0ab6b3a6a93e357e
TBScriptingFormateur <alarriger@gmail.com>
2025-09-04 10:47:30 +0200

    Update bases_variables_bool.js

--- exercices/bases_variables_bool.js
+++ exercices/bases_variables_bool.js
@@ -1,6 +1,22 @@
-function variable_array(){
+function variable_bool_or(){
 
-    var my_array = ["A","B","C","D"]
-    MessageBox.information(my_array)
+    var my_node = selection.selectedNode(0)
+    var target_name1 = "Top/group"
+    var target_name2 = "Top/Group"
+    var my_bool = (my_node == target_name1) || (my_node == target_name2)
+    MessageBox.information(my_node +" is "+target_name1+" or "+target_name2+" \n : "+my_bool)
+    MessageLog.trace(my_bool)
 
 }
+
+function variable_bool_and(){
+
+    var my_node = selection.selectedNode(0)
+    var target_name1 = "Top/group"
+    var target_name2 = "Top/Group"
+    var my_bool = (my_node == target_name1) || (my_node == target_name2)
+    MessageBox.information(my_node +" is "+target_name1+" or "+target_name2+" \n : "+my_bool)
+    MessageLog.trace(my_bool)
+
+}
+
