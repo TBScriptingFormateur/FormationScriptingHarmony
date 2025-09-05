feacf509a07e49bf29e5e1dcfb733ba2d96c0132
amunells <229730352+amunells@users.noreply.github.com>
2025-09-05 12:49:21 +0200

    Script transparency nodes

---
+++ exercices/script_transparency_nodes.js
@@ -0,0 +1,113 @@
+//V.01 Of Node creator
+
+/* The script objective is to create a automatized node creator of our choice under selected nodes of our choice.
+
+    I will set a specific part for the attribute setting.
+    The node type can be changed in type_of_node. The valid entries are, "COMPOSITE", "FADE" and all the nodes that follow a drawing type node.
+    Notice that the created nodes can be created under a "COMPOSITE" node, so I recommend to errase the condition (|| node_type == "COMPOSITE")
+    in case of creating composite nodes.
+
+    
+*/
+function node_creator(){
+
+
+    // Variables for the whole scene.
+
+    var all_nodes = selection.selectedNodes()
+    var node_type = node.type(all_nodes)
+    var node_parent = node.parentNode(all_nodes)
+    
+
+    // The name you want to show for the node 
+
+    var node_name_type = "LS_TRANSPARENCY"
+
+
+    // Allows to change the type of node by "FADE", "COMPOSITE", "READ"... (For the read node it has to be set up the column association and the elementid thing.)
+    
+    var type_of_node = "FADE"
+
+
+    // Trace the type of the selectednodes to be able to know if there was an error with any specific node or to see if there is any node in another path than the rest.
+
+    MessageLog.trace(node_type)
+    MessageLog.trace(all_nodes)
+    
+    
+    
+    
+    if (node_type == "READ" || node_type == "COMPOSITE"){
+        
+        // Creates an undo action.
+        screen.beginUndoRedoAccum()
+        
+        // Create a transparency node set up. (The type of the node is "FADE") (And the values need to be <transparency val="50" defaultValue="50"/>)
+
+        for(var i = 0; i < all_nodes; i++ ){
+        
+
+
+            // Creates a personalized name for the transparency nodes in case the name 
+
+            var final_name
+            
+            if (i = 0){
+                
+                final_name = node_name_type
+                
+            }else{
+                
+                final_name = node_name_type + "_" + i
+
+            }
+            
+
+            // Create the coordenates for each node.
+            
+            coordX = node.coordX(all_nodes[i])
+            coordY = node.coordY(all_nodes[i])
+            
+            new_coordY = coordY -50
+
+
+            //Creates the nodes. 
+
+            node.add(node_parent, final_name, type_of_node, coordX, new_coordY)
+
+
+            // if the node is linked, link the transparency node between the node that is linked to.
+            
+            if (node.isLinked(all_nodes[i]) == true){
+
+
+                // Unlinks the original link and links the new node between the two linked nodes.
+
+                node.unlink(my_node_dest,0)
+                node.link(all_node[i], 0, /*transparency_node*/, 0)
+                node.link(/*transparency_node*/, 0, /*destination_node,*/ 0)
+                
+                // find the node port to be able to link it in the same space.
+
+            }else{
+
+
+                created_node = selection.addNodeToSelection(final_name)
+                node.link(created_node, all_nodes[i])
+
+            }
+   
+        }
+        
+        screen.endUndoRedoAccum()
+
+    }else{
+
+
+        // Warning in case the selected node is not a drawing or a composite.
+
+        MessageBox.information("Plese, select Drawing or composite nodes.")
+        MessageLog.trace("Wrong node type or missing selection.")
+    }
+
+}
