e5281a45fd42630723cfbb47cfd4599c84de17e8
Frappiche <marionfrappe@orange.fr>
2025-09-09 12:40:35 +0200

    09_09_25

--- exercices/test_local_script.js
+++ exercices/test_local_script.js
@@ -4,26 +4,4 @@ function bouton_test() {
     RenameTransformationBySubstitution()
 }
 
-function FSH_show_attributes(_node, _frame) {
 
-    _node = selection.selectedNode(0)
-    _frame = Timeline.firstFrameSel
-    
-    const aframe = _frame != undefined ? _frame : frame.current()
-    var table = []
-    var names = []
-    attributes = node.getAttrList(_node, _frame)
-    for (var a in attributes) {
-        const attr = attributes[a]
-        names.push(attr.fullKeyword())
-
-        if (attr.hasSubAttributes()) {
-            sub_attr = attr.getSubAttributes()
-            for (var b in sub_attr) {
-                var sub = sub_attr[b]
-                names.push(sub.fullKeyword())
-            }
-        }
-
-    }
-}
