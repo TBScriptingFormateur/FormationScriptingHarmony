f4c7df4fa591eb43a99e6e9028f8948db5c67175
Frappiche <marionfrappe@orange.fr>
2025-09-15 16:16:56 +0200

    15/09/25

--- exercices/exo_13.js
+++ exercices/exo_13.js
@@ -1,4 +1,26 @@
-function randomiseFace(){
-    
+function randomiseFace() {
+
+   var node = selection.selectedNodes(0)
+
+    //MessageLog.trace("You have selected "+ snode)
+    /* est-ce que drawing = array et sub []?*/
+    var nodesub = node.subNodes("Top");
+    MessageLog.trace(nodesub)
+
+    /*
+    for (var nodeIndex in nodes) {
+        var layerName = nodes.getTextAttr(nodes[nodeIndex], 1, "drawing.element.layer");
+        MessageLog.trace(layerName)
+        
+        var elementId = node.getElementId(nodes[nodeIndex]);
+        for (var j = 0; j < Drawing.numberOf(elementId); j++) {
+            var drawingId = Drawing.name(elementId, j);
+            var drawingKey = Drawing.Key({ elementId: elementId, layer: layerName, exposure: drawingId });
+            DrawingTools.recolorDrawing(drawingKey, colorMapping);
+            
+        }
+    }
+    */
+
 
 }
